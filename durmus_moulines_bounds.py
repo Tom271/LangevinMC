1d8475c68ee5a19c5fdff7aaf2526987ef34c495
Tom <Tom271@users.noreply.github.com>
2019-03-01 10:57:10 +0000

    Fixed D_M bounds missing terms

--- durmus_moulines_bounds.py
+++ durmus_moulines_bounds.py
@@ -21,21 +21,26 @@ def Pi_h_error(h):
     E_1 =  2 * p / kappa * (2 * L**2 + 4 / kappa * (p * L_twiddles**2 / 3 + h * L**4  / 4) + h**2 * L**4 / 6)
     E_2 = L**4 * (4 / (3 * kappa) + h)
 
-    return (4 * np.pi)**-0.5 * ( h**2 * E_1 + 2 * p * h**2 * E_2 * (2 * p / kappa + p / m))**0.5 + 2**(-1.5) * L * (2 * p * h**3 * L**2 / (3 * kappa) + p * h**2)**0.5
+    term_1 = (4 * np.pi)**-0.5 * ( h**2 * E_1 + (2 * p * h**2 * E_2)/(h*m))
+    term_2 = (4 * np.pi)**-0.5 * np.ceil(np.log(1/h) / np.log(2)) * (h**2 * E_1 + h**2 * E_2 * (2/kappa * p + p/m))**0.5
+    term_3 = 2**(-3/2) * L * ((2 * p * h**3 * L**2)/(3*kappa) + p*h**2)**0.5
+    return term_1 + term_2 + term_3
 
 L = 1
 x_star = 0
 L_twiddles = 0
-<<<<<<< HEAD
+
 p = 2
-h = 0.01
-x = 0
+Ks = 10**4
+x = 0   
 m=1
-Ks = [10,100,1000]
-Pi_h = Pi_h_error(h)
-EM = np.array([EM_error(K, h, x) for K in Ks])
-print(EM+Pi_h)
-=======
+hs = [0.01, 0.1, 0.4, 0.49]
+Pi_h = np.array([Pi_h_error(h) for h in hs])
+EM = np.array([EM_error(Ks, h, x) for h in hs])
+print(EM + Pi_h)
+
+plt.plot(hs, EM + Pi_h)
+plt.show()
 p = 1
 #h = 0.01
 #x = 0
@@ -45,4 +50,3 @@ m=1
 # Pi_h = [Pi_h_error(h) for h in hs]
 # EM = [EM_error(K, h, x) for h in hs]
 # print(EM)
->>>>>>> 89db782d689b0f298845337dba5ccfc746cdcbac
